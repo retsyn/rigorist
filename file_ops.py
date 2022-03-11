95d522c50095b4b5d897fd5213849ca23b42114f
Matthew Riche <matthew.riche@gmail.com>
2022-03-11 16:58:50 -0500

    added loadable control .jsons

---
+++ file_ops.py
@@ -0,0 +1,41 @@
+# json.py
+# Created: Friday, 11th March 2022 3:06:37 pm
+# Matthew Riche
+# Last Modified: Friday, 11th March 2022 3:06:41 pm
+# Modified By: Matthew Riche
+
+import json
+import pymel.core as pm
+
+def dump_to_file(dict, path):
+    '''
+    Given a single dict, dump it to a file.
+    
+    Will fail if there's non-serialized content, like PyNode, PyMel attribute classes, etc.
+    '''
+
+    try:
+        with open(path, 'w') as json_file:
+            json.dump(dict, json_file)
+        return True
+
+    except:
+        print("FAILED TO WRITE {}".format(path))
+        return False
+
+
+def read_from_file(path):
+    '''
+    Read dictionary contents from JSON
+    '''
+
+    try:
+        file = open(path)
+        data = json.load(file)
+
+        file.close()
+
+        return data
+    except:
+        print("FAILED TO READ {}".format(path))
+        return None
